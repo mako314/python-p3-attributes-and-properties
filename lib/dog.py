91d11a1fbee1b38b978598dc869021e74ebc7904
Macolister <bispo.mac@gmail.com>
2023-06-15 19:43:23 -0400

    write properties and attributes for classes

--- lib/dog.py
+++ lib/dog.py
@@ -11,5 +11,30 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
-class Dog:
-    pass
+class Dog():
+    def __init__(self, name = "Maki", breed = "Mastiff"):
+        if isinstance(name, str) and (1<=len(name) <=25):
+            self.name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+        if isinstance(breed,str) and breed in APPROVED_BREEDS:
+            self.breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+        @property
+        def name(self):
+            return self._name
+        
+        @name.setter
+        def name(self, new_name):
+            self._name = new_name
+
+        @property
+        def breed(self):
+            return self._breed
+        
+        @breed.setter
+        def breed(self, new_breed):
+            self._breed = new_breed
--- lib/person.py
+++ lib/person.py
@@ -15,5 +15,31 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
-class Person:
-    pass
+class Person():
+    def __init__(self, name = "Maky", job = "Engineer"):
+        if isinstance(name, str) and (1<=len(name)<=25):
+            self.name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+        
+        if isinstance(job, str) and job in APPROVED_JOBS:
+            self.job = job
+        else:
+            print("Job must be in list of approved jobs.")
+        
+
+        @property
+        def name(self):
+            return self._name
+        
+        @name.setter
+        def name(self, new_name):
+            self._name = new_name
+        
+        @property
+        def job(self):
+            return self._job
+        
+        @job.setter
+        def job(self, new_job):
+            self._job = new_job
